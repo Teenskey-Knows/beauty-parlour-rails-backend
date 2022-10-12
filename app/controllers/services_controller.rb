1d2dce639c00c609f4b5628348400362df0fe729
Njeri Ngungu <janetnjerin89@gmail.com>
2022-10-12 20:19:51 +0300

    created error handling

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,16 +1,19 @@
 class ServicesController < ApplicationController
+ rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+ rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     def index
         service = Service.all 
         render json: service 
     end
 def create
+
     service = Service.create!(service_params)
     render json: service, status: :created
 end
 def update 
     service = find_service
-    service.update (service_params)
+    service.update!(service_params)
     render json: service
 end
 def destroy
@@ -22,11 +25,18 @@ end
 private
 
 def find_service
-    Service.find(params[:id])
+    service = Service.find(params[:id])
 end
 
 def service_params
     params.permit(:service_type, :pricing, :image)
 end
 
+def render_not_found_response
+    render json: { error: "Service not found" }, status: :not_found
+end
+
+def render_unprocessable_entity_response(exception)
+    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity 
+end 
 end
