01bcfdd87fd8a2776ce7cb9dd594cb23e41b55cd
Njeri Ngungu <janetnjerin89@gmail.com>
2022-10-12 19:17:56 +0300

    add create and route

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -4,5 +4,15 @@ class ServicesController < ApplicationController
         service = Service.all 
         render json: service 
     end
+def create
+    service = Service.create!(service_params)
+    render json: service, status: :created
+end
+
+private
+
+def service_params
+    params.permit(:service_type, :pricing, :image)
+end
 
 end
