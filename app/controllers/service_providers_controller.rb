9da2acffac14cac1c078e9d6aaa7ff4351065cf4
Njeri Ngungu <janetnjerin89@gmail.com>
2022-10-12 22:28:37 +0300

    create create and destroy methods, and routes

--- app/controllers/service_providers_controller.rb
+++ app/controllers/service_providers_controller.rb
@@ -1,8 +1,28 @@
 class ServiceProvidersController < ApplicationController
 
      def index
-        service = ServiceProvider.all 
-        render json: service 
+        service_provider = ServiceProvider.all 
+        render json: service_provider 
     end
-    
+    def create
+
+    service_provider = ServiceProvider.create!(service_provider_params)
+    render json: service_provider, status: :created
+end
+def destroy
+    service_provider = find_service_provider
+    service_provider.destroy
+    head:no_content
+end
+private
+
+def find_service_provider
+    ServiceProvider.find(params[:id])
+end
+
+def service_provider_params
+    params.permit(:name, :gender)
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 resources :services, only: [:index, :create, :update, :destroy]
 
-resources :service_providers, only: [:index]
+resources :service_providers, only: [:index, :create, :destroy]
 #bookings controller
  resources :bookings, only: [:index,:create,:update,:destroy]
 
